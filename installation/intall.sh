51fb75c601c80316b660ce2fc2e0c36722d82e1b
cerff-gur-sheel <ayslan.gamedev@dgmail.com>
2025-07-27 20:51:18 -0300

    some features

--- installation/intall.sh
+++ installation/intall.sh
@@ -65,7 +65,101 @@ echo "mouting partitions ..."
 	mount --mkdir "$EFI_DEV" /mnt/boot
 )
 
-pacstrap -K /mnt base base-devel linux linux-firmware linux-headers btrfs-progs grub efibootmgr zram-generator networkmanager git 
+echo "[INFO] Configuring Pacman"
+(
+	pacman-key --init 
+	pacman-key --populate archlinux
+	cat > /etc/pacman.conf << PACMANCONF
+#
+# /etc/pacman.conf
+#
+# See the pacman.conf(5) manpage for option and repository directives
+
+#
+# GENERAL OPTIONS
+#
+[options]
+RootDir     = /
+DBPath      = /var/lib/pacman/
+CacheDir    = /var/cache/pacman/pkg/
+LogFile     = /var/log/pacman.log
+GPGDir      = /etc/pacman.d/gnupg/
+HookDir     = /etc/pacman.d/hooks/
+HoldPkg     = pacman glibc
+#XferCommand = /usr/bin/curl -L -C - -f -o %o %u
+#XferCommand = /usr/bin/wget --passive-ftp -c -O %o %u
+CleanMethod = KeepInstalled
+Architecture = auto
+
+#
+# Ignored Packages
+#
+
+#IgnorePkg   =
+#IgnoreGroup =
+
+#NoUpgrade   =
+#NoExtract   =
+
+
+#
+# Misc options
+#
+
+Color
+CheckSpace
+ParallelDownloads = 15
+DownloadUser = alpm
+#NoProgressBar
+#DisableSandbox
+#VerbosePkgLists
+#UseSyslog
+ILoveCandy
+
+SigLevel    = Required DatabaseOptional
+LocalFileSigLevel = Optional
+
+#
+# REPOSITORIES
+#
+
+[core]
+Include = /etc/pacman.d/mirrorlist
+
+[extra]
+Include = /etc/pacman.d/mirrorlist
+
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+
+#[core-testing]
+#Include = /etc/pacman.d/mirrorlist
+
+#[extra-testing]
+#Include = /etc/pacman.d/mirrorlist
+
+#[multilib-testing]
+#Include = /etc/pacman.d/mirrorlist
+	PACMANCONF
+	pacman -Syu
+	pacman-key --init 
+  pacman-key --populate archlinux
+	pacman -Syy
+)
+
+pacstrap -K /mnt base base-devel linux linux-firmware linux-headers \
+	btrfs-progs grub efibootmgr zram-generator networkmanager git \
+	mesa vulkan-intel lib32-vulkan-intel \
+	pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
+	alsa-tools alsa-utils \
+	nerd-fonts noto-fonts noto-fonts-emoji \
+	gnome-keyring libsecret gnome-themes-extra \
+	nwg-look qt5ct qt6ct \
+	hyprland waybar wofi hyprpicker hyprlock swww kitty fish wl-clipboard \
+	steam telegram-desktop strawberry btop ly \
+	nemo nemo-fileroller nemo-preview nemo-seahorse nemo-share nemo-terminal \
+	krita opentabletdriver 
+
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cp ~/rice/installation/chroot_config.sh /mnt/chroot_config.sh
